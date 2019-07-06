75ba44902c88a618e8b698fc6a52f326b75a474f
Ross Perkel <rossperkel@gmail.com>
2019-07-06 03:47:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,18 +2,9 @@ def my_map(array)
   i = 0
   newArr = []
   while i < array.length do
-    newArr.push( yield(array[i]))    
-    i+=1
+    newArr.push( yield(array[i]) )    
+    i += 1
   end
   return newArr
 end
 
-def map(source_array)
-  new = []
-  i = 0
-  while i < source_array.length do
-    new.push( yield(source_array[i]) ) # <== Unique work
-    i += 1
-  end
-  return new
-end
