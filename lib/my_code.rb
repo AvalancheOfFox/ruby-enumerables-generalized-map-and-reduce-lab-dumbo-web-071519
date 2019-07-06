2fa7c4efdc3882da2f8d8d49e635def2bda52924
Ross Perkel <rossperkel@gmail.com>
2019-07-06 03:46:38 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,3 +1,12 @@
+def my_map(array)
+  i = 0
+  newArr = []
+  while i < array.length do
+    newArr.push( yield(array[i]))    
+    i+=1
+  end
+  return newArr
+end
 
 def map(source_array)
   new = []
